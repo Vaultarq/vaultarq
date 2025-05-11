44bdadc705d93fbeefba00668ee7bbf8e74be485
okwareddevnest <softengdedan@gmail.com>
2025-05-12 00:01:25 +0300

    Update all SDK versions to 0.1.6

--- sdks/python-sdk/setup.py
+++ sdks/python-sdk/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="vaultarq",
-    version="0.1.5",
+    version="0.1.6",
     author="Dedan Okware",
     author_email="softengdedan@gmail.com",
     description="Python SDK for Vaultarq - The developer-first, invisible secrets manager",
